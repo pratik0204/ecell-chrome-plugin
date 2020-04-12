eee7c980b7348ea21a4689b1dcf44a8a3e14953c
Pratik <pchy.393@gmail.com>
2020-04-12 14:16:05 +0530

    ui improved

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, "toggle");
+    chrome.tabs.sendMessage(tab.id, "toggle");
 });
 
 
@@ -7,44 +7,44 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 var id = 100;
 
 chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.name == 'clicked') {
-      chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-          var viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
-          var targetId = null;
+    if (request.name == 'clicked') {
+        chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+            var viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
+            var targetId = null;
 
-          chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
+            chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
 
-              if (tabId != targetId || changedProps.status != "complete")
-                  return;
+                if (tabId != targetId || changedProps.status != "complete")
+                    return;
 
-              chrome.tabs.onUpdated.removeListener(listener);
+                chrome.tabs.onUpdated.removeListener(listener);
 
-              var views = chrome.extension.getViews();
-              for (var i = 0; i < views.length; i++) {
-                  var view = views[i];
-                  if (view.location.href == viewTabUrl) {
-                      view.setScreenshotUrl(screenshotUrl);
-                      break;
-                  }
-              }
-          });
+                var views = chrome.extension.getViews();
+                for (var i = 0; i < views.length; i++) {
+                    var view = views[i];
+                    if (view.location.href == viewTabUrl) {
+                        view.setScreenshotUrl(screenshotUrl);
+                        break;
+                    }
+                }
+            });
 
-          var views = chrome.extension.getViews();
-          for (var i = 0; i < views.length; i++) {
-              var view = views[i];
-              if (view.location.href == viewTabUrl) {
-                  view.setScreenshotUrl(screenshotUrl);
-                  break;
-              }
-          }
+            var views = chrome.extension.getViews();
+            for (var i = 0; i < views.length; i++) {
+                var view = views[i];
+                if (view.location.href == viewTabUrl) {
+                    view.setScreenshotUrl(screenshotUrl);
+                    break;
+                }
+            }
 
-          chrome.tabs.create({ url: viewTabUrl }, function(tab) {
-              targetId = tab.id;
-          });
+            chrome.tabs.create({ url: viewTabUrl }, function(tab) {
+                targetId = tab.id;
+            });
 
-          sendResponse({ ssUrl: screenshotUrl });
-      });
+            sendResponse({ ssUrl: screenshotUrl });
+        });
 
-      return true;
-  }
+        return true;
+    }
 })
